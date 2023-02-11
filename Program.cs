d056c8485988ec428dd559461bbe3a165e664889
azamat yusupov <ayi-azamat1990@mail.ru>
2023-02-11 19:16:39 +0500

    попытка номер 5

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a,b;
+ Console.WriteLine("ВВудите два чесла");
+ a = Convert.ToInt32(Console.ReadLine());
+ b = Convert.ToInt32(Console.ReadLine());
+ if (a>b)
+ {
+    Console.WriteLine("{0} наибольшее чесло",a);
+
+ }
+ else
+ {
+    Console.WriteLine("{0}наибольшее чесло",b);
+ }
+ Console.ReadLine();
+
+
+
+
+
+
+  
